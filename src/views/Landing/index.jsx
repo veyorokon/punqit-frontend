06a0be9252483babe9103f46106182fc7c47ef4d
Vahid <veyorokon@gmail.com>
2019-11-11 14:37:22 -0500

    Hero section.

--- src/views/Landing/index.jsx
+++ src/views/Landing/index.jsx
@@ -4,12 +4,22 @@ import styled from "styled-components";
 
 const TitleContainer = styled(Flex)`
   flex-grow: 1;
+  z-index: 10;
 `;
 const VideoContainer = styled(Box)`
   overflow: hidden;
-  margin: auto 0 40rem 4rem !important;
+  margin: auto 5rem 30rem 4rem !important;
+  position: absolute;
+  right: 0;
+  bottom: 0;
 `;
 const Video = styled.video``;
+const Title = styled(Text)`
+  height: fit-content;
+`;
+const SubTitle = styled(Text)`
+  line-height: 1;
+`;
 
 class Landing extends React.Component {
   render() {
@@ -20,9 +30,32 @@ class Landing extends React.Component {
       }
     };
     return (
-      <Flex bg={"blacks.0"} width={"100vw"} height={"100vh"}>
-        <TitleContainer>
-          <Text color={"whites.0"}>punq</Text>
+      <Flex bg={"blacks.0"} width={"100vw"} height={"90rem"}>
+        <TitleContainer
+          flexDirection={"column"}
+          mt={"15rem"}
+          ml={"10%"}
+          mb={"auto"}
+        >
+          <Flex>
+            <Title fs={"12rem"} fw={"400"} as="h1" color={"whites.0"}>
+              punqit
+            </Title>
+            <Text fs={"12rem"} fw={"bold"} color={"red"}>
+              .
+            </Text>
+          </Flex>
+          <Flex mt={5} w={"fit-content"}>
+            <SubTitle
+              lineHeight="1"
+              as="p"
+              color={"whites.0"}
+              fs={"3rem"}
+              fw={"300"}
+            >
+              Deepfakes for the web.
+            </SubTitle>
+          </Flex>
         </TitleContainer>
         <VideoContainer width={"110rem"} height={"60rem"} bg="blacks.0">
           <Video
@@ -33,8 +66,9 @@ class Landing extends React.Component {
             muted
             type="video/mp4"
             style={{
-              width: "120rem",
+              width: "130rem",
               marginLeft: "4rem",
+              marginTop: "3rem",
               height: "auto",
               outline: "none"
             }}
