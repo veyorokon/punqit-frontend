8608366faac51803cf3f069179dfa1124afc7498
Vahid <veyorokon@gmail.com>
2020-01-10 18:11:17 -0500

    Hero complete.

--- src/views/Landing/Sections/Hero.js
+++ src/views/Landing/Sections/Hero.js
@@ -6,26 +6,20 @@ import {
   Animate,
   Video,
   Hidden as CHidden,
-  Button
+  Button,
+  ImageBox
 } from "components";
-import styled, {keyframes} from "styled-components";
+import styled from "styled-components";
 import {responsive as r} from "lib";
-
-const appear = keyframes`
-0%{
-  opacity: 0;
-}
-100%{
-  opacity: 1;
-}
-`;
+const heroBgMobile = "/images/hero-black-2.jpg";
+const MobileImage = `url(${heroBgMobile})`;
 
 const Section = styled(Flex)`
-  transition: padding 0.3s ease-in-out;
+  transition: height 0.2s ease-in-out;
 `;
 
 const Hidden = styled(CHidden)`
-  transition: display 0.4s ease-in-out;
+  transition: display 0.3s ease-in-out;
 `;
 const TitleContainer = styled(Flex)`
   max-width: fit-content;
@@ -38,7 +32,6 @@ const VideoContainer = styled(Box)`
   transition: top 0.4s ease-in-out;
 `;
 const Title = styled(Text)`
-  height: 100%;
   transition: all 0.3s ease-in-out;
 `;
 const SubTitle = styled(Text)`
@@ -50,10 +43,13 @@ const CallToAction = styled(Button)`
   color: black;
   background: white;
   border: 2px solid white !important;
-  transition: all 0.3s ease-in-out;
+  transition: background 0.3s ease-in-out;
+  display: flex;
+  justify-content: center;
   &:hover {
     background: black;
     color: white;
+    cursor: pointer;
   }
   &:focus {
     outline: none;
@@ -63,7 +59,7 @@ const CallToAction = styled(Button)`
 class Hero extends React.Component {
   render() {
     const videoOptions = {
-      src: "video-bg-trimmed.mp4",
+      src: "/video-bg-trimmed.mp4",
       ref: videoRef => {
         this.videoRef = videoRef;
       }
@@ -72,103 +68,106 @@ class Hero extends React.Component {
       <Section
         p={r("2 ------> 3")}
         overflow="hidden"
-        bg={"blacks.0"}
         width={"100vw"}
         height={"90rem"}
+        background={r("blacks.1 -----> blacks.0")}
       >
-        <TitleContainer
-          h={"40rem"}
-          flexDirection={"column"}
-          ml={r("auto ----> 5")}
-          mr={r("auto ----> 0")}
-          mt={r("3 4 -------> 5")}
+        <ImageBox
+          flexDirection="column"
+          p={r("2 ------> 3")}
+          backgroundImage={[
+            MobileImage,
+            MobileImage,
+            MobileImage,
+            MobileImage,
+            MobileImage,
+            MobileImage,
+            "none"
+          ]}
+          width={"100%"}
+          justifyContent={["center", "center", "center", "flex-start"]}
+          backgroundSize={r("80rem -> 90rem -----> 100rem 120rem")}
+          backgroundPosition={"left"}
+          backgroundPositionY={r("20rem ------> center")}
+          backgroundPositionX={[
+            "-46rem",
+            "-46rem",
+            "-46rem",
+            "-46rem",
+            "-46rem",
+            "-46rem",
+            "-46rem",
+            "-38rem",
+            "-44rem"
+          ]}
         >
-          <Animate
-            animation={appear}
-            duration={"1.5s"}
-            function={"ease-in-out"}
-            width={"fit-content"}
+          <TitleContainer
+            h={r("35rem -> 40rem")}
+            flexDirection={"column"}
+            ml={r("auto -----> 5")}
+            mr={r("auto -----> 0")}
+            mt={r("3 4 -------> 5")}
           >
-            <Flex>
-              <Title
-                fs={r("7rem 8rem ---> 10rem --> 12rem 12.5rem")}
-                fw={"400"}
-                as="h1"
-                color={"whites.0"}
-                mr={1}
-              >
-                punqit
-              </Title>
-              <Hidden bp={4} down>
+            <Animate duration={"1.5s"} width={"fit-content"} margin="auto">
+              <Flex>
                 <Title
                   fs={r("7rem 8rem ---> 10rem --> 12rem 12.5rem")}
-                  fw={"bold"}
-                  color={"red"}
+                  fw={"400"}
+                  as="h1"
+                  color={"whites.0"}
                 >
-                  .
+                  punqit
                 </Title>
-              </Hidden>
-            </Flex>
-          </Animate>
-          <Flex mt={4} mb={1} w={"fit-content"}>
-            <Animate
-              delay={"0.5s"}
-              animation={appear}
-              duration={"1.5s"}
-              function={"ease-in-out"}
-            >
-              <SubTitle
-                lineHeight="1"
-                as="p"
-                color={"whites.0"}
-                fs={r("2rem 2.2rem ---> 2.6rem --> 2.8rem 3.5rem")}
-                fw={"300"}
-              >
-                Make deepfakes online
-              </SubTitle>
+                <Hidden bp={5} down>
+                  <Title
+                    fs={r("7rem 8rem ---> 10rem --> 12rem 12.5rem")}
+                    fw={"bold"}
+                    color={"red"}
+                  >
+                    .
+                  </Title>
+                </Hidden>
+              </Flex>
             </Animate>
-          </Flex>
-          <Flex
-            ml={r("auto ----> unset")}
-            mr={r("auto ----> 0")}
-            mt={r("6 ----> 4")}
-            w={"fit-content"}
-          >
-            <Hidden bp={4} up>
-              <Animate
-                delay={"0.75s"}
-                animation={appear}
-                duration={"1.5s"}
-                function={"ease-in-out"}
-              >
-                <CallToAction color={"black"} bg={"whites.0"}>
-                  <Text color={"inherit"} fs={"1.8rem"}>
-                    Start
-                  </Text>
-                </CallToAction>
+            <Flex mt={4} mb={1} w={"fit-content"}>
+              <Animate delay={"0.5s"}>
+                <SubTitle
+                  lineHeight="1"
+                  as="p"
+                  color={"whites.0"}
+                  fs={r("2rem 2.2rem ---> 2.6rem --> 2.8rem 3.5rem")}
+                  fw={"300"}
+                >
+                  Make deepfakes online
+                </SubTitle>
               </Animate>
-            </Hidden>
-            <Hidden bp={4} down>
-              <Animate
-                delay={"0.75s"}
-                animation={appear}
-                duration={"1.5s"}
-                function={"ease-in-out"}
-              >
-                <CallToAction color={"black"} bg={"whites.0"}>
+            </Flex>
+            <Flex
+              ml={r("auto -----> unset")}
+              mr={r("auto -----> 0")}
+              mt={r("6 -----> 4")}
+              w={"fit-content"}
+            >
+              <Animate delay={"0.75s"}>
+                <CallToAction
+                  br={"5px"}
+                  h="5rem"
+                  color={"black"}
+                  bg={"whites.0"}
+                >
                   <Text color={"inherit"} fs={"2rem"}>
                     Get Started
                   </Text>
                 </CallToAction>
               </Animate>
-            </Hidden>
-          </Flex>
-        </TitleContainer>
+            </Flex>
+          </TitleContainer>
+        </ImageBox>
 
-        <Hidden down bp={4}>
+        <Hidden down bp={5}>
           <VideoContainer
             right="0"
-            top={r("30% -------> 20%")}
+            top={r("27% -------> 20%")}
             mt={"auto"}
             w={"110rem"}
             h={"60rem"}
